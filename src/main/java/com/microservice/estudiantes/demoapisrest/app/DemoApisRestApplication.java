eac38179b7635c8111a85878c0768a6298ef4516
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-11-15 10:01:27 -0500

    init

---
+++ src/main/java/com/microservice/estudiantes/demoapisrest/app/DemoApisRestApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.estudiantes.demoapisrest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApisRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApisRestApplication.class, args);
+    }
+
+}
